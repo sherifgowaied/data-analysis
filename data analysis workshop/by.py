811d4024fe53e41e084cebcf24ab1ad852429e48
sherifgowaied <91483223+sherifgowaied@users.noreply.github.com>
2023-03-03 21:07:22 +0200

    Add files via upload

---
+++ data analysis workshop/by.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+hourly_data = pd.read_csv('data analysis book and projects/Bike-Sharing-Dataset/hour.csv')
+
+preprocessed_data=hourly_data.copy()
+
+
